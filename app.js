f34055a7619549ada6ac08284363fc10c53d14c9
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-02-15 17:35:27 +0530

    Added delayKeyUp.. Again!

--- app.js
+++ app.js
@@ -5,47 +5,51 @@
 
 (function () {
     window.onload = function () {
-        var html = '', map = document.getElementById('map-canvas'), latLng = new google.maps.LatLng(21.7679, 78.8718), infowindow,users = [],markers = []
-        options = {
-            center: latLng,
-            zoom: 5,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true,
-            zoomControl: true,
-            disableDoubleClickZoom: true,
-            scrollwheel: true,
-        },
-		map = new google.maps.Map(map, options);
-		google.maps.Map.prototype.clearMarkers = function() {
-		for (var i = 0; i < markers.length; i++ ) {
-				markers[i].setMap(null);
-		  }
-		  markers = new Array();
-		};
+        var html = '',
+            map = document.getElementById('map-canvas'),
+            latLng = new google.maps.LatLng(21.7679, 78.8718),
+            infowindow, users = [],
+            markers = []
+            options = {
+                center: latLng,
+                zoom: 5,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true,
+                zoomControl: true,
+                disableDoubleClickZoom: true,
+                scrollwheel: true,
+            },
+            map = new google.maps.Map(map, options);
+        google.maps.Map.prototype.clearMarkers = function () {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
+            markers = new Array();
+        };
 
         $.getJSON("data.json", function (data) {
-			$.each(data, function (key, user) {
-				users.push(user);
-				html += '<li class="row" data-id="'+key+'"><div class="col-md-4"><img src="'+(user.picture)+'"></div><div class="col-md-8"><span><a href="javascript:" class="m">'+(user.name)+'</a></span><br/><span>'+(user.team)+'</span><br/><span>'+(user.email)+'</span></div></li>';			
-			   setTimeout(function () {
+            $.each(data, function (key, user) {
+                users.push(user);
+                html += '<li class="row" data-id="' + key + '"><div class="col-md-4"><img src="' + (user.picture) + '"></div><div class="col-md-8"><span><a href="javascript:" class="m">' + (user.name) + '</a></span><br/><span>' + (user.team) + '</span><br/><span>' + (user.email) + '</span></div></li>';
+                setTimeout(function () {
                     draw(user);
-                }, 125 + ( key * 125));				
+                }, 125 + (key * 125));
             });
-			$("#results").html(html);		
-			
-        });		
-		
-		window.setTimeout(function(){
-		$(".m").on("click",function(){
-		
-			map.clearMarkers();
-					$("#rp").prop("checked",true);
-			$('#search').val($.trim($(this).text())).trigger('keyup');
-					$("#rp").prop("checked",false);
-		});
-		
-		
-		},521);
+            $("#results").html(html);
+
+        });
+
+        window.setTimeout(function () {
+            $(".m").on("click", function () {
+
+                map.clearMarkers();
+                $("#rp").prop("checked", true);
+                $('#search').val($.trim($(this).text())).trigger('keyup');
+                $("#rp").prop("checked", false);
+            });
+
+
+        }, 125);
 
         var draw = function (user) {
             var marker = new google.maps.Marker({
@@ -54,7 +58,7 @@
                 title: user.name + " (" + user.phone + ") ",
                 animation: google.maps.Animation.DROP,
             });
-			markers.push(marker);
+            markers.push(marker);
             var contentString = '<div id="content">' +
                 '<div id="siteNotice">' +
                 '</div>' +
@@ -63,64 +67,66 @@
                 '<p><b>' + user.email + ' | ' + user.phone + '</b><br/>' + user.about + '</p>' +
                 '</div>' +
                 '</div>';
-		      google.maps.event.addListener(marker, 'click', function () {
-				    if (!infowindow) {
-                        infowindow = new google.maps.InfoWindow();
-                    }
-                    infowindow.setContent(contentString);
-                    infowindow.open(map, marker);									
-				});
-				
+            google.maps.event.addListener(marker, 'click', function () {
+                if (!infowindow) {
+                    infowindow = new google.maps.InfoWindow();
+                }
+                infowindow.setContent(contentString);
+                infowindow.open(map, marker);
+            });
+
         };
-    
-		$('#search').on("keyup",function(){	
-			var isChecked = $("#rp").is(":checked");
-			if(isChecked || $.trim($(this).val()).length == 0 ) map.clearMarkers();
-		   var valThis = $(this).val().toLowerCase();
-			$('#results>li').each(function(key, val){
-			 var text = $(this).text().toLowerCase();
-				
-				if(text.indexOf(valThis) >= 0) 
-				{
-					var _self = $(this);
-					_self.show();
-					if(isChecked || $.trim($("#search").val()).length == 0)
-					{
-						setTimeout(function () {
-							draw(users[parseInt(_self.data("id"))]);
-						}, 125 + ( key * 125));
-					}
-				}
-				else
-				{
-					$(this).hide();
-				}
-		   });
-		   // redraw the markers
-		   	
-		});
 
-	
-		$("#rp").on("change", function(){
-			if($(this).is(":checked"))
-			{
-				$("#rplt").hide();
-			}
-			else
-			{
-				$("#rplt").show();
-			}		
-		});
-		$("#rplt").click(function(){
-			$("#rp").prop("checked",true);
-			if($.trim($('#search').val()).length > 0) $('#search').trigger("keyup");		
-			$("#rp").prop("checked",false);
-		});
-		
-		
+        $('#search').delayKeyup(function (ele) {
+            var isChecked = $("#rp").is(":checked");
+            if (isChecked || $.trim($(ele).val()).length == 0) map.clearMarkers();
+            var valThis = $(ele).val().toLowerCase();
+            $('#results>li').each(function (key, val) {
+                var text = $(this).text().toLowerCase();
 
-	};
-	
-	
+                if (text.indexOf(valThis) >= 0) {
+                    var _self = $(this);
+                    _self.show();
+                    if (isChecked || $.trim($("#search").val()).length == 0) {
+                        setTimeout(function () {
+                            draw(users[parseInt(_self.data("id"))]);
+                        }, 125 + (key * 125));
+                    }
+                } else {
+                    $(this).hide();
+                }
+            });
+            // redraw the markers
+
+        }, 999);
+
+
+        $("#rp").on("change", function () {
+            if ($(this).is(":checked")) {
+                $("#rplt").hide();
+            } else {
+                $("#rplt").show();
+            }
+        });
+        $("#rplt").click(function () {
+            $("#rp").prop("checked", true);
+            if ($.trim($('#search').val()).length > 0) $('#search').trigger("keyup");
+            $("#rp").prop("checked", false);
+        });
+
+
+
+    };
+    //http://code.azerti.net/javascript/jquery/delaykeyup.htm
+    (function ($) {
+        $.fn.delayKeyup = function (callback, ms) {
+            var timer = 0;
+            $(this).keyup(function () {
+                clearTimeout(timer);
+                timer = setTimeout(callback($(this)), ms);
+            });
+            return $(this);
+        };
+    })(jQuery);
 
 })();
